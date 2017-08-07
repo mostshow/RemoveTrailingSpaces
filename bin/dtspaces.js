e109eca37b5b977088634cafd0a000fb992dd942
lyndon <mostshow@users.noreply.github.com>
2017-08-07 10:47:46 +0800

    readline

--- bin/dtspaces.js
+++ bin/dtspaces.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 
-const drf= require('../index.js');
+const dtspaces= require('../index.js');
 const program = require('commander');
 const args = process.argv.slice(2);
 const path = process.cwd();
@@ -10,4 +10,30 @@ program
   .version('0.1.0')
   .usage(' dtspaces <dir>')
   .parse(process.argv);
-console.log(args)
+const run = function(args){
+    if(args.length < 1){
+
+        console.error(' Usage: dtspaces source');
+
+        process.exit(1);
+    }else{
+	    try {
+	        dtspaces(args[0], function (err) {
+	            if (Array.isArray(err)) {
+	                console.error('There were errors during the contrast.');
+	                err.forEach(function (err) {
+	                    console.error(err.stack || err.message);
+	                });
+	            }else if (err) {
+	                console.error('An error has occurred.');
+	                console.error(err.stack || err.message);
+	            }
+	        });
+	    } catch (e) {
+	        console.error((e))
+	        process.exit(1);
+	    }
+	}
+};
+
+run(args)
